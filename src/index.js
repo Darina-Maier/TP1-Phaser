09cbb74a6037608a5503b8dbbea489826ac45ba8
Darina Maier <darina.maier@epfedu.fr>
2026-02-18 11:05:13 +0100

    TP2 terminé

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 /** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
 /***********************************************************************/
 
+
 // configuration générale du jeu
 var config = {
   type: Phaser.AUTO,
@@ -49,6 +50,13 @@ function preload() {
   });
   this.load.image("img_etoile", "src/assets/star.png");
   this.load.image("img_bombe", "src/assets/bomb.png");
+
+
+  // chargement tuiles de jeu
+  this.load.image("Phaser_tuilesdejeu", "src/assets/tuilesJeu.png");
+
+  // chargement de la carte
+  this.load.tilemapTiledJSON("carte", "src/assets/map.json");
 }
 
 /***********************************************************************/
@@ -62,14 +70,6 @@ function preload() {
  * ainsi que toutes les instructions permettant de planifier des evenements
  */
 function create() {
-  this.add.image(400, 300, "img_ciel");
-
-  groupe_plateformes = this.physics.add.staticGroup();
-  groupe_plateformes.create(200, 584, "img_plateforme");
-  groupe_plateformes.create(600, 584, "img_plateforme");;
-  groupe_plateformes.create(50, 300, "img_plateforme");
-  groupe_plateformes.create(600, 450, "img_plateforme");
-  groupe_plateformes.create(750, 270, "img_plateforme");
 
   player = this.physics.add.sprite(100, 450, 'img_perso');
   player.setCollideWorldBounds(true);
@@ -79,6 +79,7 @@ function create() {
   clavier = this.input.keyboard.createCursorKeys();
 
   this.anims.create({
+
     key: "anim_tourne_gauche",  // key est le nom de l'animation : doit etre unique poru la scene.
     frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }), // on prend toutes les frames de img perso numerotées de 0 à 3 
     frameRate: 10,  // vitesse de défilement des frames 
@@ -100,25 +101,66 @@ function create() {
 
   groupe_etoiles = this.physics.add.group();
   for (var i = 0; i < 10; i++) {
-      var coordX = 70 + 70 * i;
-      groupe_etoiles.create(coordX, 10, "img_etoile");
-    }
-  this.physics.add.collider(groupe_etoiles, groupe_plateformes) ; 
+    var coordX = 70 + 70 * i;
+    groupe_etoiles.create(coordX, 10, "img_etoile");
+  }
+  this.physics.add.collider(groupe_etoiles, groupe_plateformes);
 
   groupe_etoiles.children.iterate(function iterateur(etoile_i) {
-    var coef_rebond = Phaser.Math.FloatBetween(0.4,0.8) ;
-    etoile_i.setBounceY(coef_rebond) ;
-    });  
-  
-  this.physics.add.overlap(player,groupe_etoiles,ramasserEtoile,null,this);
+    var coef_rebond = Phaser.Math.FloatBetween(0.4, 0.8);
+    etoile_i.setBounceY(coef_rebond);
+  });
 
-  zone_texte_score = this.add.text(16,16,'score: 0',{ fontSize :'32px',fill: '#000'}); 
+  this.physics.add.overlap(player, groupe_etoiles, ramasserEtoile, null, this);
 
-  groupe_bombes = this.physics.add.group(); 
-  this.physics.add.collider(groupe_bombes, groupe_plateformes); 
+  zone_texte_score = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-  this.physics.add.collider(player, groupe_bombes, chocAvecBombe, null, this) ;  
-  }
+  groupe_bombes = this.physics.add.group();
+  this.physics.add.collider(groupe_bombes, groupe_plateformes);
+
+  this.physics.add.collider(player, groupe_bombes, chocAvecBombe, null, this);
+
+  // chargement de la carte
+  const carteDuNiveau = this.add.tilemap("carte");
+
+  // chargement du jeu de tuiles
+  const tileset = carteDuNiveau.addTilesetImage(
+    "tuiles_de_jeu",
+    "Phaser_tuilesdejeu"
+  );
+
+  // chargement du calque calque_background
+  const calque_background = carteDuNiveau.createLayer(
+    "calque_background",
+    tileset
+  );
+
+  // chargement du calque calque_background_2
+  const calque_background_2 = carteDuNiveau.createLayer(
+    "calque_background_2",
+    tileset
+  );
+
+  // chargement du calque calque_plateformes
+  const calque_plateformes = carteDuNiveau.createLayer(
+    "calque_plateformes",
+    tileset
+  );
+
+  calque_plateformes.setCollisionByProperty({ estSolide: true });
+
+  // ajout d'une collision entre le joueur et le calque plateformes
+  this.physics.add.collider(player, calque_plateformes);
+  // redimentionnement du monde avec les dimensions calculées via tiled
+  this.physics.world.setBounds(0, 0, 3200, 640);
+  //  ajout du champs de la caméra de taille identique à celle du monde
+  this.cameras.main.setBounds(0, 0, 3200, 640);
+  // ancrage de la caméra sur le joueur
+  this.cameras.main.startFollow(player);
+
+  player.setDepth(100);
+
+}
 
 
 /***********************************************************************/
@@ -139,51 +181,51 @@ function update() {
 
   }
 
-  if (clavier.space.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+  if (clavier.space.isDown && player.body.blocked.down) {
+    player.setVelocityY(-400);
   }
 
   if (gameOver) {
-        return;
-    }
+    return;
+  }
 }
 
 function ramasserEtoile(un_player, une_etoile) {
-  une_etoile.disableBody(true,true);
+  une_etoile.disableBody(true, true);
   if (groupe_etoiles.countActive(true) == 0) {
     groupe_etoiles.children.iterate(function iterateur(etoile_i) {
       etoile_i.enableBody(true, etoile_i.x, 0, true, true);
     });
   }
-  score += 10; ;
-  zone_texte_score.setText("Score:"+ score);
+  score += 10;;
+  zone_texte_score.setText("Score:" + score);
 
   var x;
-  if(player.x<400){
-    x=Phaser.Math.Between(400,800);
-  }else{
-    x=Phaser.Math.Between(0,400);
+  if (player.x < 400) {
+    x = Phaser.Math.Between(400, 800);
+  } else {
+    x = Phaser.Math.Between(0, 400);
   }
 
-  var une_bombe = groupe_bombes.create(x,16,"img_bombe");
+  var une_bombe = groupe_bombes.create(x, 16, "img_bombe");
   une_bombe.setBounce(1);
   une_bombe.setCollideWorldBounds(true);
-  une_bombe.setVelocity(Phaser.Math.Between(-200,200),20);
+  une_bombe.setVelocity(Phaser.Math.Between(-200, 200), 20);
   une_bombe.allowGravity = false;
 }
 
-function chocAvecBombe(un_player,une_bombe) {
+function chocAvecBombe(un_player, une_bombe) {
   this.physics.pause();
   player.setTint(0xff0000);
-  player.anims.play("anim_face") ;
+  player.anims.play("anim_face");
   gameOver = true;
-} 
+}
 
 var groupe_plateformes;
 var player;  // désigne le sprite du joueur 
 var clavier;
 var groupe_etoiles;
-var score = 0 ;
-var zone_texte_score; 
-var groupe_bombes;  
+var score = 0;
+var zone_texte_score;
+var groupe_bombes;
 var gameOver = false; 
